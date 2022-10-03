2e06db4f311f7e3f15ae83eab1c8374217eee946
canmandrac <canmandrac1996@gmail.com>
2022-10-03 11:32:12 +0300

    v1
    iptv-v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { OwlOptions } from "ngx-owl-carousel-o";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'iptv';
+  title = "iptv";
+  customOptions: OwlOptions = {
+    loop: true,
+    autoplay: true,
+    center: true,
+    dots: false,
+    autoHeight: true,
+    autoWidth: true,
+    navSpeed: 700,
+    navText: [" < ", " > "],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+    nav: true,
+  };
+
+  scroll(id: string) {
+    let el = document.getElementById(id);
+    setTimeout(() => {
+      window.scrollTo({ top: el["offsetTop"] - 50, behavior: "smooth" });
+    }, 0);
+  }
 }
